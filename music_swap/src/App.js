97a8dce926ca3d57d0279815216af4f849d968cd
skiislam <sisla016@ucr.edu>
2024-07-01 19:40:45 -0700

    Implicit Grant Flow Done

--- music_swap/src/App.js
+++ music_swap/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import authorize from './scripts/authorize.js'
+import {useEffect, useState} from "react"
 function App() {
+  
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem('token')
+    if(!token && hash){
+      token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('-')[1]
+
+      window.location.hash = ''
+      window.localStorage.setItem('token', token)
+      setToken(token)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Welcome to Song Swap</h2>
+      
+      <button onClick = {authorize}>Login into Spotify</button>
+
     </div>
   );
 }
---
+++ music_swap/src/scripts/authorize.js
@@ -0,0 +1,15 @@
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+  const REDIRECT_URI = 'http://localhost:3000/callback'
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+
+function authorize()
+ { let url = AUTH_ENDPOINT;
+  url +='?response_type=token';
+  url +='&client_id=' + CLIENT_ID;
+  url +='&scope= user-read-email user-read-private'
+  url +='&redirect_uri=' + encodeURI(REDIRECT_URI);
+  url +='&show_dialog=true';
+  window.location.href = url
+}
+export default authorize;
